bdf56593ea553770f9b837bd2397290b5f29ed3d
KP125689 <P.RWEHUMBIZA-2021@hull.ac.uk>
2023-05-21 17:14:54 +0100

    testing out selenium before implementation

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # this is a basic web scraper that counts how many times a word appears on a website
 
 import requests
+from bs4 import BeautifulSoup
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 
 response = requests.get('https://en.wikipedia.org/wiki/Computer_science')
 print("connected to website")
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup.title)
+
+driver = Chrome(executable_path='/path/to/driver')
+driver.get('https://oxylabs.io/blog')
+
